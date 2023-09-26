a7548416555e609e0f2fc9513288bdb8678ac1d5
Tuckkrub00 <145004375+Tuckkrub00@users.noreply.github.com>
2023-09-26 19:12:10 +0700

    delet login

--- frontend/public/loginpage/lgscript.js
+++ frontend/public/loginpage/lgscript.js
@@ -1,10 +1,10 @@
 import { getid,getid02 } from '../fetch_api/api.js'
 
-const submit = document.getElementById("login-submit");
-const nextsubmit = document.getElementById("login01");
-nextsubmit.addEventListener('click', abcde);
 
-function abcde(){
-  console.log("hello word");
+const nextsubmit = document.getElementById("login01");
+nextsubmit.addEventListener('click', singin);
+
+function singin(){
+  console.log("hello word01");
   getid02();
 }
