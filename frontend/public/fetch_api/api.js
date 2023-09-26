e25ab23c661d236c0372b85322767e60e0bdf421
TanThanaphat <tan.thanaphat4@gmail.com>
2023-09-26 19:52:47 +0700

    Update api.js

--- frontend/public/fetch_api/api.js
+++ frontend/public/fetch_api/api.js
@@ -1,10 +1,11 @@
-
  export async function getid(id_user, month) {
      let s = fetch(`http://localhost:5000/api/users/login`,)
     
  }
 
  export async function getid02(id_user, month) {
-    console.log("hi");
+    let Username = document.querySelector("#Username").value
+    let Password = document.querySelector("#Password").value
+    console.log(Username, Password)
 }
 
