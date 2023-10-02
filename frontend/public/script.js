b175baa1d7d7930d6379bef55a9b6e0b0881fbf7
TanThanaphat <tan.thanaphat4@gmail.com>
2023-10-02 17:21:51 +0700

    Fixed Something

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -178,7 +178,65 @@ TodoListAdd_Button.addEventListener("click", () => {
 const LoginButton = document.querySelector(".submit");
 
 LoginButton.addEventListener('click', () => {
-    console.log("hi");
-    let s = fetch(`http://localhost:5000/api/users/login`,)
-    console.log(s.status)
+    let Username = document.querySelector(".username > input").value
+    let Password = document.querySelector(".password > input").value
+    data = {userName: Username, password: Password}
+    console.log(Username, Password);
+    let id = Login(Username, Password);
+    console.log(id);
 });
+
+async function Login(Username, Password) {
+    const body = {
+        "userName":Username,
+        "password":Password
+    };
+    let s = await fetch('http://localhost:5000/api/users/login', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+    return s.json();
+}
+
+async function createitem() {
+    const body = {
+    "day": 21,
+    "month":7,
+    "year":2566,
+    "id_user":"64fe94545920f7fa3f9fdbe6",
+    "todo":["กินมาม่า",
+            "ทำการบ้าน",
+            "อ่านหนังสือ"]
+    };
+    await fetch('http://localhost:5000/api/items/creact', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+    return ;
+ }
+
+async function createitem() {
+    const body = {
+    "day": 21,
+    "month":7,
+    "year":2566,
+    "id_user":"64fe94545920f7fa3f9fdbe6",
+    "todo":["กินมาม่า",
+            "ทำการบ้าน",
+            "อ่านหนังสือ"]
+    };
+    await fetch('http://localhost:5000/api/items/creact', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+    return ;
+ }
