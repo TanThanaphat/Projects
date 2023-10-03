601d888a5324d01a7f8338d9b7c992a554e0ad21
Taspol <taspolsd@gmail.com>
2023-10-04 01:23:09 +0700

    loginpage update

---
+++ frontend/public/login/script.js
@@ -0,0 +1,15 @@
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  }
+
+  function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  }
+  function showpic() {
+    document.getElementById('pan').style.display = "block";
+  }
+  function closepic() {
+    document.getElementById("pan").style.display = "none";
+  }
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -294,3 +294,17 @@ async function createuser() {
       });
     return monthitembox.json();
  }
+ 
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+function showpic() {
+      document.getElementById('pan').style.display = "block";
+}
+function closepic() {
+      document.getElementById("pan").style.display = "none";
+}
